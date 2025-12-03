2d807e4e80abdcd1cf42b1f16511889c38b9aee3
Rocabor <robertoborgesjimez@gmail.com>
2025-12-03 19:55:42 -0400

    corrections

--- apps/junior/newsletter-sing-up-with-success-message/script.js
+++ apps/junior/newsletter-sing-up-with-success-message/script.js
@@ -1,11 +1,14 @@
-// METODO API FormData
+const form = document.getElementById("form-email");
 
-document.getElementById("form-email").addEventListener("submit", function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const messError = document.getElementById("message-error");
   const input = document.getElementById("email");
   const email = input.value;
+  const main = document.getElementById("main-page");
+  const success = document.getElementById("container-success");
+  const eUser = document.getElementById("e-user");
 
   function validarEmail(email) {
     // Expresión regular básica para validar el formato de un email
@@ -13,28 +16,19 @@ document.getElementById("form-email").addEventListener("submit", function (event
     return regex.test(email); // El método test() devuelve true o false
   }
 
-  // Función que aplica o quita el estilo de error
-  function manejarEstiloError(esValido) {
-    if (!esValido) {
-      // Si NO es válido:
-      input.classList.add("input-error"); // Añade la clase CSS de error
-      messError.innerHTML = `Valid email requerid`;
-      messError.style.display = "block"; // Muestra el mensaje de error
-    } else {
-      // Si es válido:
-      input.classList.remove("input-error"); // Quita la clase CSS de error
-      messError.style.display = "none"; // Oculta el mensaje de error
-    }
+  if (validarEmail(email) === false) {
+    // Si NO es válido:
+    input.classList.add("input-error");
+    messError.innerHTML = `Valid email requerid`;
+    messError.style.display = "block";
+  } else {
+    // Si es válido:
+    main.style.display = "none";
+    success.style.display = "block";
+    eUser.textContent = email;
   }
 
-  // Añadimos el Event Listener para el evento 'blur' (cuando el usuario sale del input
-  input.addEventListener("blur", function () {
-    const email = this.value;
-    const esValido = validarEmail(email);
-    manejarEstiloError(esValido);
-  });
-
-  // Opcional: Limpiar el estilo de error si el usuario vuelve a hacer clic para corregir
+  // Limpiar el estilo de error si el usuario vuelve a hacer clic para corregir
   input.addEventListener("focus", function () {
     this.classList.remove("input-error");
     messError.style.display = "none";
