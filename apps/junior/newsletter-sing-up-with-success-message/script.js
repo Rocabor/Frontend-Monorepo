847572fb4caefdfbb48400cedf0f27424ada6e9d
Rocabor <robertoborgesjimez@gmail.com>
2025-12-05 12:05:51 -0400

    challenge finish

--- apps/junior/newsletter-sing-up-with-success-message/script.js
+++ apps/junior/newsletter-sing-up-with-success-message/script.js
@@ -4,9 +4,16 @@ const success = document.getElementById("container-success");
 const eUser = document.getElementById("e-user");
 const messError = document.getElementById("message-error");
 const input = document.getElementById("email");
+const btn = document.getElementById("subscribe");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  // FUNCION DE VALIDACION DEL EMAIL
+  function validarEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email); // El método test() devuelve true o false
+  }
 
   const email = input.value;
 
@@ -16,31 +23,41 @@ form.addEventListener("submit", function (event) {
     setTimeout(function () {
       input.classList.remove("highlight-effect");
     }, 220);
+  } else if (validarEmail(email)) {
+    console.log("valido");
+    main.style.display = "none";
+    success.style.display = "flex";
+    eUser.textContent = email;
+    input.classList.remove("input-error");
+    messError.innerHTML = "";
   } else {
-    function validarEmail(email) {
-      // Expresión regular básica para validar el formato de un email
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return regex.test(email); // El método test() devuelve true o false
-    }
+    console.log("Es inválido. ");
+    input.classList.add("input-error");
+    messError.innerHTML = `Valid email requerid`;
+    messError.style.display = "block";
+  }
+  
+});
 
-    if (validarEmail(email) === false) {
-      // Si NO es válido:
-      input.classList.add("input-error");
-      messError.innerHTML = `Valid email requerid`;
-      messError.style.display = "block";
-    } else {
-      // Si es válido:
-      main.style.display = "none";
-      success.style.display = "flex";
-      eUser.textContent = email;
-      input.classList.remove("input-error");
-      messError.innerHTML = "";
-    }
+// ACTIVACION BOTON SUBSCRIBE EMAIL VALIDO
+
+input.addEventListener("input", function () {
+  const email = input.value;
+  if (email.includes("@") && email.includes(".com") && email.length > 5) {
+    btn.classList.add("btn-focus");  
   }
 });
+
+// FUNCION DE BOTON DISSMISS
 
 function dissmiss() {
   success.style.display = "none";
   main.style.display = "flex";
   form.reset();
+  btn.classList.remove("btn-focus");
 }
+
+const dismissBtn = document.getElementById("dismissBtn");
+dismissBtn.addEventListener("click", dissmiss);
+
+
