f7ab5303e10a98e3bff840629b18cc6a5fc9d475
Rocabor <robertoborgesjimez@gmail.com>
2026-05-04 13:37:48 -0400

    chore: add Vite setup and project configuration

---
+++ apps/junior/interactive-pricing-component/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-cyan-900">
+    <h1 class="text-4xl font-bold text-white underline">
+      ¡Tailwind v4 funcionando!
+    </h1>
+  </div>
+</template>
