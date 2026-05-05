136ef2bfbfc207938b1c985026d63f2dbb9d2ac0
Rocabor <robertoborgesjimez@gmail.com>
2026-05-05 19:08:47 -0400

    feat: add header with background pattern and typography

--- apps/junior/interactive-pricing-component/src/App.vue
+++ apps/junior/interactive-pricing-component/src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-cyan-900">
-    <h1 class="text-4xl font-bold text-white underline">
-      ¡Tailwind v4 funcionando!
-    </h1>
-  </div>
+  <header class="h-100 bg-[url(images/bg-pattern.svg)] bg-cover bg-no-repeat xl:h-112.25 xl:bg-size-[100%_100%]">
+    <div
+      class="relative mx-auto flex w-81.5 translate-y-22.25 flex-col gap-2 bg-[url(images/pattern-circles.svg)] bg-center bg-no-repeat text-center before:absolute before:top-1/2 before:left-1/2 before:z-[-1] before:size-36.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-[url(images/pattern-circles.svg)] before:content-[''] md:w-98.75 md:gap-3 
+      md:translate-y-16.75">
+
+      <h1 class="preset-1">Simple, traffic-based pricing</h1>
+
+      <p class="preset-2">
+        Sign-up for our 30-day trial.
+        <br class="md:hidden" />
+        No credit card required.
+      </p>
+    </div>
+  </header>
 </template>
