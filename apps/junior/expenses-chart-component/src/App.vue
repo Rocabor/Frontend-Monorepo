da025645e36115463a234c4c97e0fdab3283fa83
Rocabor <robertoborgesjimez@gmail.com>
2026-05-14 17:46:42 -0400

    feat: Add hover/active states and tooltip to chart bars

--- apps/junior/expenses-chart-component/src/App.vue
+++ apps/junior/expenses-chart-component/src/App.vue
@@ -3,57 +3,58 @@ import { MyFooter } from '@packages/ui';
 import data from '../data.json';
 
 const chartData = data;
-const maxAmount = Math.max(...chartData.map(item => item.amount));
-
+const maxAmount = Math.max(...chartData.map((item) => item.amount));
 </script>
 
 <template>
-
   <!--* Balance section -->
-  <div class="flex w-85.75 md:w-135 flex-col gap-4 md:6">
+  <div class="flex w-85.75 flex-col gap-4 md:w-135 md:gap-6">
 
     <!--* Balance container -->
-    <div class="flex w-full rounded-[10px] md:rounded-[20px] bg-red-500 px-5.5 py-5.75
-    md:px-[35.5px] md:py-6.5">
-      
+    <div class="flex w-full rounded-[10px] bg-red-500 px-5.5 py-5.75 md:rounded-[20px] md:px-[35.5px] md:py-6.5">
+
       <!--* Balance info -->
       <div class="flex w-full items-center justify-between">
         <div class="flex flex-col md:gap-2">
           <p class="text-preset-5-reg">My balance</p>
           <p class="text-preset-3">$921.48</p>
         </div>
-        <img src="./assets/images/logo.svg" alt="" class="h-10 w-15 md:w-18 md:h-12" aria-hidden="true"/>
+        <img src="./assets/images/logo.svg" alt="" class="h-10 w-15 md:h-12 md:w-18" aria-hidden="true" />
       </div>
     </div>
 
     <!--* Spending section -->
-    <div class="rounded-[10px] md:rounded-[20px] bg-white px-4 py-6 w-full md:px-10 md:py-9.5">
+    <div class="w-full rounded-[10px] bg-white px-4 py-6 md:rounded-[20px] md:px-10 md:py-9.5">
+
       <!--* Spending container -->
       <div class="flex flex-col gap-14 md:gap-16">
-        
 
         <!--* Bar chart container -->
-        <figure class="flex flex-col gap-6 md:gap-8">
-
-          <figcaption class="text-preset-3 text-brown-950">Spending - Last 7 days</figcaption>
+        <figure class="flex flex-col">
+          <figcaption class="text-preset-3 text-brown-950 mb-14 md:mb-16">Spending - Last 7 days</figcaption>
 
           <!--* Bar chart -->
-          <ul class="mx-auto grid h-43.5 max-w-112.5 w-full grid-cols-7 items-end gap-2">
-            
+          <ul class="mx-auto grid min-h-43.5 w-full max-w-112.5 grid-cols-7 items-end gap-2 md:gap-4">
             <!-- iteraciones del archivo data.json para los dias de la semana -->
-            <li v-for="item in chartData" :key="item.day" class="flex h-full flex-col items-center justify-end gap-2">
-              <div 
-              :style="{height:(item.amount/maxAmount*100)+'%'}" 
-              :class="[item.amount === maxAmount ?  'bg-blue-300' : 'bg-red-500']" class="w-full rounded-sm"></div>
-              <p class="text-preset-6">{{ item.day }}</p>
-              <span class="visually-hidden" >amount: ${{ item.amount }}</span>
-            </li>
+            <li v-for="item in chartData" :key="item.day" class="group flex h-full cursor-pointer flex-col items-center justify-end gap-2">
+              <div
+                :style="{ height: (item.amount / maxAmount) * 100 + '%' }"
+                :class="[item.amount === maxAmount ? 'bg-blue-300 group-hover:bg-blue-200 group-active:bg-blue-200' : 'bg-red-500 group-hover:bg-red-300 group-active:bg-red-300']"
+                class="w-full rounded-sm relative">
+                <!-- Amount visible en hover -->
+                <span class="bg-brown-950 absolute -top-11 left-1/2 -translate-x-1/2 rounded-[5px] p-2 text-preset-4-bold text-white opacity-0 transition-opacity group-hover:opacity-100 group-active:opacity-100">
+                  ${{ item.amount }}
+                  </span>
 
-            
+                
+              </div>
+              <p class="text-preset-6">{{ item.day }}</p>
+            </li>
           </ul>
 
           <!--* Total section -->
-          <div class="border-t-2 border-red-100">
+          <div class="border-t-2 border-red-100 mt-6 md:mt-8">
+
             <!--* Total container -->
             <div class="mt-6 md:mt-8">
               <p class="text-preset-5-reg text-brown-400">Total this month</p>
