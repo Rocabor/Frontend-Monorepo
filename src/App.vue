6d6a493a4a8cb68db3b944035535bb94c58accd9
Rocabor <robertoborgesjimez@gmail.com>
2026-06-07 16:55:31 -0400

    fix(bmi-calculator): remove Results import until component is created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import { MyFooter } from '@packages/ui';
 import Hero from './components/Hero.vue';
-import Results from './components/Results.vue';
 </script>
 
 <template>
   <Hero />
-  <Results/>
 </template>
---
+++ src/components/Results.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col gap-12 md:flex-row md:h-[411px] md:-translate-x-[40px]  md:gap-18 border">
+
+    <!--* Image Container -->
+    
+      <img src="../assets/images/image-man-eating.webp" alt="" class=" -translate-x-8 md:w-[361px] md:h-[411px] md:-translate-x-0" />
+   
+
+    <!--* Results Text Container -->
+    <article class="flex flex-col gap-8 w-[328px] mx-auto md:w-[331px] md:h-[342px] md:my-auto md:mx-0">
+
+      <h2 class="text-preset-3">What your BMI result means</h2>
+
+      <p class="text-preset-6-regular ">A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
+
+      
+    </article>
+  </div>
+</template>
