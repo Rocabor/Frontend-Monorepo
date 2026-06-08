306ca92c1aa59e10a2ebd322de2a2448bde16a71
Rocabor <robertoborgesjimez@gmail.com>
2026-06-08 14:55:57 -0400

    feat(bmi-calculator): add Tips component with SVG icons and semantic improvements

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { MyFooter } from '@packages/ui';
 import Hero from './components/Hero.vue';
 import Results from './components/Results.vue';
+import Tips from './components/Tips.vue';
 </script>
 
 <template>
   <Hero />
   <Results/>
+  <Tips/>
 </template>
