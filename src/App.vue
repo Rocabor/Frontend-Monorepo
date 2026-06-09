b237ae42a3552c6305b94c02f44c5265b6176091
Rocabor <robertoborgesjimez@gmail.com>
2026-06-09 17:36:54 -0400

    feat(junior): Add header with theme toggle and dark mode support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { MyFooter } from '@packages/ui'; 
+import { MyFooter } from '@packages/ui';
+import Header from './components/header.vue';
+
 </script>
 
 <template>
-  
+  <Header/>
 </template>
