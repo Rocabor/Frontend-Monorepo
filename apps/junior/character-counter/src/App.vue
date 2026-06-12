6d8bfefc30c9dc02a3ddd77bbc545588544becd3
Rocabor <robertoborgesjimez@gmail.com>
2026-06-12 16:27:18 -0400

    feat: add Stats component with character, word, and sentence counts

--- apps/junior/character-counter/src/App.vue
+++ apps/junior/character-counter/src/App.vue
@@ -2,10 +2,12 @@
 import { MyFooter } from '@packages/ui';
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
+import Stats from './components/Stats.vue';
 
 </script>
 
 <template>
   <Header/>
   <Main/>
+  
 </template>
