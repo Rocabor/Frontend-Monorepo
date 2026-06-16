27d71ad0ce6c2a7fe52b674e67ab8596e6ea4428
Rocabor <robertoborgesjimez@gmail.com>
2026-06-16 10:13:03 -0400

    feat:Add focus-visible styles for keyboard navigation

--- apps/junior/conference-ticket-generator/src/components/Form.vue
+++ apps/junior/conference-ticket-generator/src/components/Form.vue
@@ -28,33 +28,27 @@ const formFields = [
       <label>Upload Avatar</label>
 
       <!--* Drag and drop container -->
-      <div
-        class="bg-neutral-0/5 relative flex h-31.5 aspect-[2.72/1] w-full cursor-pointer flex-col items-center justify-center gap-4 rounded-xl border-2 border-dashed border-neutral-500 transition-colors hover:border-neutral-500"
-        :class="{ 'border-neutral-500': !filePreviewUrl && fileInputRef?.matches(':hover') }"
-        @click="() => fileInputRef?.click()"
-        @dragover.prevent
-        @drop="handleDrop">
+      <button
+        type="button"
+        class="bg-neutral-0/5 relative flex h-31.5 aspect-[2.72/1] w-full cursor-pointer flex-col items-center justify-center gap-4 rounded-xl border-2 border-dashed border-neutral-500 transition-colors hover:bg-neutral-0/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900">
+
         <div
           class="bg-neutral-0/10 flex size-12.5 items-center justify-center rounded-xl border border-neutral-700 shadow-[0_2px_4px_-2px_rgba(4,1,40,1)] backdrop-blur-sm">
           <img src="../assets/images/icon-upload.svg" alt="" class="aspect-square w-1/2" />
         </div>
 
         <p class="text-preset-4 text-center text-neutral-300">Drag and drop or click to upload</p>
+       
 
-        <input type="file" ref="fileInputRef" class="sr-only" accept=".jpg, .jpeg, .png" @change="onFileChange" />
-
-        <div
-          v-if="filePreviewUrl"
-          class="absolute inset-0 rounded-xl bg-cover bg-center"
-          :style="{ backgroundImage: `url(${filePreviewUrl})` }"></div>
-      </div>
+        
+      </button>
 
       <!--* Hint text -->
       <div class="flex items-center gap-2">
         <img src="../assets/images/icon-info.svg" alt="" class="size-3" />
         <p class="text-preset-5 text-neutral-300">Upload your photo (JPG or PNG, max size: 500KB).</p>
       </div>
-      <p v-if="fileError" class="text-preset-5 mt-1 text-orange-500">{{ fileError }}</p>
+      
     </div>
 
     <!--* Text input -->
@@ -65,12 +59,12 @@ const formFields = [
         :type="field.type"
         :id="field.id"
         :placeholder="field.placeholder"
-        class="text-preset-4 h-13.5 w-full placeholder-neutral-300 rounded-xl border border-neutral-500 bg-white/8 px-4 focus:border-neutral-500 focus:outline-none" />
+        class="text-preset-4 h-13.5 w-full placeholder-neutral-300 rounded-xl border border-neutral-500 bg-white/8 px-4 hover:bg-neutral-0/20  focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900 " />
     </div>
 
     <button
       type="submit"
-      class="text-preset-6 flex h-13.5 w-full items-center justify-center rounded-xl bg-orange-500 text-neutral-900 transition-colors hover:bg-orange-700">
+      class="text-preset-6 flex h-13.5 w-full items-center justify-center rounded-xl bg-orange-500 text-neutral-900 transition-colors hover:bg-orange-700 hover:inset-shadow-[0_-4px_0_rgba(245,116,99,1)] focus:outline-offset-3 focus:outline-2 focus:outline-neutral-0">
       Generate My Ticket
     </button>
   </form>
