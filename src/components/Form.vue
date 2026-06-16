9844a98e36738c6bd4fee8c10ba288d486ddce0e
Rocabor <robertoborgesjimez@gmail.com>
2026-06-16 13:58:48 -0400

    feat: add avatar upload with preview and remove/change image buttons

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -19,6 +19,30 @@ const formFields = [
     className: 'md:col-span-2',
   },
 ];
+
+const fileinput = ref(null);
+const avatarFile = ref(null);
+const avatarPreview = ref(null);
+
+function openFilePicker () {
+  fileinput.value.click();
+}
+function changeImage() {
+  fileinput.value.click()  
+}
+function removeImage() {
+  avatarFile.value = null
+  avatarPreview.value = null
+  fileinput.value.value = ''  
+}
+
+
+function handleFileChange(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+  avatarFile.value = file;
+  avatarPreview.value = URL.createObjectURL(file);
+}
 </script>
 
 <template>
@@ -27,20 +51,35 @@ const formFields = [
     <div class="flex w-full flex-col gap-3">
       <label>Upload Avatar</label>
 
+      <!--* upload file -->
+      <input ref="fileinput" type="file" accept="image/png, image/jpeg" @change="handleFileChange" class="sr-only" />
+
       <!--* Drag and drop container -->
       <button
         type="button"
-        class="bg-neutral-0/5 relative flex h-31.5 aspect-[2.72/1] w-full cursor-pointer flex-col items-center justify-center gap-4 rounded-xl border-2 border-dashed border-neutral-500 transition-colors hover:bg-neutral-0/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900">
+        @click="openFilePicker"
+        class="bg-neutral-0/5 hover:bg-neutral-0/20 relative flex aspect-[2.72/1] h-31.5 w-full cursor-pointer flex-col items-center justify-center gap-4 rounded-xl border-2 border-dashed border-neutral-500 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900">
 
         <div
-          class="bg-neutral-0/10 flex size-12.5 items-center justify-center rounded-xl border border-neutral-700 shadow-[0_2px_4px_-2px_rgba(4,1,40,1)] backdrop-blur-sm">
+          v-if="!avatarPreview"
+          class="bg-neutral-0/10 flex size-12.5 items-center justify-center rounded-xl border-2 border-neutral-700 shadow-[0_2px_4px_-2px_rgba(4,1,40,1)] backdrop-blur-sm">
           <img src="../assets/images/icon-upload.svg" alt="" class="aspect-square w-1/2" />
         </div>
 
-        <p class="text-preset-4 text-center text-neutral-300">Drag and drop or click to upload</p>
-       
+        <div
+          v-else
+          class="bg-neutral-0/10 flex size-12.5 items-center justify-center rounded-xl border-2 border-neutral-500 shadow-[0_2px_4px_-2px_rgba(4,1,40,1)] backdrop-blur-sm">
+          <img :src="avatarPreview" alt="" class="size-full rounded-xl object-cover" />
+        </div>
 
-        
+        <div v-if="avatarPreview" class="w-[180px] h-[22px] flex gap-2 text-preset-7b">
+
+          <button type="button" @click="removeImage" class="underline underline-offset-2 text-neutral-300 bg-neutral-0/10 px-2 py-1 rounded-sm cursor-pointer">Remove image</button>
+          
+          <button type="button" @click="changeImage" class="text-neutral-0 bg-neutral-0/10 px-2 py-1 rounded-sm cursor-pointer">Change image</button>
+        </div>
+
+        <p v-else class="text-preset-4 text-center text-neutral-300">Drag and drop or click to upload</p>
       </button>
 
       <!--* Hint text -->
@@ -48,7 +87,6 @@ const formFields = [
         <img src="../assets/images/icon-info.svg" alt="" class="size-3" />
         <p class="text-preset-5 text-neutral-300">Upload your photo (JPG or PNG, max size: 500KB).</p>
       </div>
-      
     </div>
 
     <!--* Text input -->
@@ -59,12 +97,12 @@ const formFields = [
         :type="field.type"
         :id="field.id"
         :placeholder="field.placeholder"
-        class="text-preset-4 h-13.5 w-full placeholder-neutral-300 rounded-xl border border-neutral-500 bg-white/8 px-4 hover:bg-neutral-0/20  focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900 " />
+        class="text-preset-4 hover:bg-neutral-0/20 h-13.5 w-full rounded-xl border border-neutral-500 bg-white/8 px-4 placeholder-neutral-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-3 focus-visible:ring-offset-neutral-900" />
     </div>
 
     <button
       type="submit"
-      class="text-preset-6 flex h-13.5 w-full items-center justify-center rounded-xl bg-orange-500 text-neutral-900 transition-colors hover:bg-orange-700 hover:inset-shadow-[0_-4px_0_rgba(245,116,99,1)] focus:outline-offset-3 focus:outline-2 focus:outline-neutral-0">
+      class="text-preset-6 focus:outline-neutral-0 flex h-13.5 w-full items-center justify-center rounded-xl bg-orange-500 text-neutral-900 transition-colors hover:bg-orange-700 hover:inset-shadow-[0_-4px_0_rgba(245,116,99,1)] focus:outline-2 focus:outline-offset-3">
       Generate My Ticket
     </button>
   </form>
