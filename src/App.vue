8915f88db000f21148ced4a95b4c30a9a85c5474
Rocabor <robertoborgesjimez@gmail.com>
2026-06-20 14:10:22 -0400

    refactor: simplify getURLImage to extract filename directly

--- src/App.vue
+++ src/App.vue
@@ -6,41 +6,34 @@ const products = ref([]);
 
 // Función mágica de Vite para resolver rutas dinámicas desde src/assets
 const getURLImage = (pathInJson) => {
-
-  const cleanPath = pathInJson.replace('./assets/', 'assets/');
-  return new URL(cleanPath, import.meta.url).href;
-}
+  const fileName = pathInJson.split('/').pop();
+  return new URL(`./assets/images/${fileName}`, import.meta.url).href;
+};
 
 onMounted(async () => {
   try {
     const data = await fetch('data.json');
     const rawProducts = await data.json();
-    
+
     // MAPEAMOS los productos para transformar los strings estáticos en URLs dinámicas de Vite
-    products.value = rawProducts.map(product => ({
+    products.value = rawProducts.map((product) => ({
       ...product,
       image: {
         thumbnail: getURLImage(product.image.thumbnail),
         mobile: getURLImage(product.image.mobile),
         tablet: getURLImage(product.image.tablet),
-        desktop: getURLImage(product.image.desktop)
-      }
+        desktop: getURLImage(product.image.desktop),
+      },
     }));
-
   } catch (error) {
     console.error('Error cargando los postres:', error);
   }
 });
-
-
-
-
-
 </script>
 
 <template>
   <!-- * Main Content -->
-  <main class="flex flex-col gap-8 m-6">
+  <main class="m-6 flex flex-col gap-8">
     <h1>Desserts</h1>
     <Desserts :products="products" />
   </main>
