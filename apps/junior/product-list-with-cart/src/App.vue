5ebc175a90384a734a3908e94efc9aa9a083b1ec
Rocabor <robertoborgesjimez@gmail.com>
2026-06-20 12:03:26 -0400

    feat: add Desserts component with product data fetching

--- apps/junior/product-list-with-cart/src/App.vue
+++ apps/junior/product-list-with-cart/src/App.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import Desserts from './components/Desserts.vue';
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    const data = await fetch('data.json');
+    products.value = await data.json();
+  } catch (error) {
+    console.error('Error cargando los postres:', error);
+  }
+});
+
+
+
+</script>
 
 <template>
-  <main />
+  <!-- * Main Content -->
+  <main class="flex flex-col gap-8 m-6">
+    <h1>Desserts</h1>
+    <Desserts :products="products" />
+  </main>
 </template>
