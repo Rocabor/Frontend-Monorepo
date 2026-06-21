463a457d5b9b65a49a62ea1130fb2863afccc80b
Rocabor <robertoborgesjimez@gmail.com>
2026-06-20 23:17:47 -0400

    feat: add responsive grid layout and Cart component with text-preset utilities

---
+++ src/components/Cart.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="flex min-h-[226px] flex-col gap-6 rounded-[12px] bg-white p-6 md:col-span-3 xl:row-start-1 xl:col-start-4 xl:h-[299px] xl:row-span-4 xl:ml-2 xl:col-span-1">
+    <h2 class="text-preset-4">Your Cart {{}}</h2>
+
+    <div class="flex min-h-[195px] flex-col items-center gap-4 py-4">
+      <img
+        src="../assets/images/illustration-empty-cart.svg"
+        alt=""
+        class="size-[128px]" />
+      <p class="text-preset-2 text-rose-500">Your added items will appear here</p>
+    </div>
+  </div>
+</template>
--- src/components/Desserts.vue
+++ src/components/Desserts.vue
@@ -14,29 +14,37 @@ defineProps({
   <div
     v-for="product in products"
     :key="product.name"
-    class="flex flex-col items-center gap-4 mb-2">
-
+    class="mb-2   gap-4 md:mb-0">
     <!--* Item Image and Add To Cart Button -->
-    <div class="relative h-[234px] ">
-      <img
-        :src="product.image.mobile"
-        :alt="product.name"
-        class="h-[212px] rounded-[8px]" />
+    <div class="relative h-[234px] xl:h-[262px]">
+      <picture>
+        <source media="(min-width:1440px)" :srcset="product.image.desktop" />
+        <source media="(min-width:768px)" :srcset="product.image.tablet" />
+        <img
+          :src="product.image.mobile"
+          :alt="product.name"
+          class="h-[212px] rounded-[8px] xl:h-[240px] " />
+      </picture>
 
-      <button class="h-[44px] w-[160px] rounded-full border border-rose-400 bg-white p-3 absolute flex justify-center items-center bottom-0 left-1/2 -translate-x-1/2">
+      <button
+        class="text-preset-2 absolute bottom-0 left-1/2 flex h-[44px] w-[160px] -translate-x-1/2 items-center justify-center gap-2 rounded-full border border-rose-400 bg-white p-3">
         <img
           :src="iconAddtoCart"
-          alt="icon cart" 
-          class="size-5 inline"/>
+          alt="icon cart"
+          class="inline size-5" />
         Add to Cart
       </button>
     </div>
 
     <!-- * Item Info -->
-    <div class="flex flex-col gap-1 w-full">
-      <p>{{ product.category }}</p>
-      <p>{{ product.name }}</p>
-      <p>${{ product.price }}</p>
+    <div class="flex h-[69px] w-full flex-col gap-1">
+      <p class="text-preset-2 font-normal text-rose-500">
+        {{ product.category }}
+      </p>
+      <p class="text-preset-3">
+        {{ product.name }}
+      </p>
+      <p class="text-preset-3 text-red">${{ product.price.toFixed(2) }}</p>
     </div>
   </div>
 </template>
