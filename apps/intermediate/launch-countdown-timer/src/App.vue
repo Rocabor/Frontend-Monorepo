0bae5e5276f3e37c6fa95adc4fbb8d81b36899ae
Rocabor <robertoborgesjimez@gmail.com>
2026-07-01 14:14:49 -0400

    refactor: simplify App.vue layout and enhance main.css for responsive design

--- apps/intermediate/launch-countdown-timer/src/App.vue
+++ apps/intermediate/launch-countdown-timer/src/App.vue
@@ -3,12 +3,7 @@ import { MyFooter } from '@packages/ui';
 </script>
 
 <template>
-  <main class="mx-auto mt-[142px] w-[327px]">
-    <h1 class="text-preset-1 text-center">We’re launching soon</h1>
-
-    <div></div>
-    <div></div>
-  </main>
+  <main></main>
 
   <MyFooter hidden />
 </template>
