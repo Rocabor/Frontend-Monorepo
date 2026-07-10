4d420aa55c3cc924606e97a1a4f142ad50fd6ee0
Rocabor <robertoborgesjimez@gmail.com>
2026-07-10 18:38:01 -0400

    feat: add ProfileCard component and update App.vue layout

--- apps/junior/github-user-search-app/src/App.vue
+++ apps/junior/github-user-search-app/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { MyFooter } from '@packages/ui';
+import ProfileCard from './components/ProfileCard.vue';
 </script>
 
 <template>
+  <ProfileCard />
   <MyFooter class="tracking-[-0.7px]" />
 </template>
